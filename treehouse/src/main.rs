a611668d9b88c8ad2bab94a03230a8af459365e2
aquaflamingo <16901597+aquaflamingo@users.noreply.github.com>
2022-05-05 18:16:00 -0700

    Adds iterators, functions and structs, oh my

--- treehouse/src/main.rs
+++ treehouse/src/main.rs
@@ -1,3 +1,54 @@
+use std::io::stdin;
+
+struct Visitor {
+    name: String,
+    greeting: String,
+}
+
+impl Visitor {
+    fn new(n: &str, g: &str) -> Self {
+        Self {
+            name: n.to_lowercase(),
+            greeting: g.to_string(),
+        }
+    }
+
+    fn greet(&self) {
+        println!("{}", self.greeting);
+    }
+}
+
+
+fn whats_ur_name() -> String {
+    let mut name = String::new();
+
+    stdin()
+        .read_line(&mut name)
+        .expect("Failed to read line"); 
+
+    name
+        .trim()
+        .to_lowercase()
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello what is your name?");
+    let name = whats_ur_name();
+    let mut allow_them_in = false;
+
+    let the_list = [
+        Visitor::new("robert", "how u doing Robert"), 
+        Visitor::new("dave", "Good to see you dave"), 
+        Visitor::new("frankie", "Who invited Frankie!"),
+    ];
+
+    let known = the_list.iter().find(|v| v.name == name);
+
+    // TODO 
+
+    if allow_them_in {
+        println!("Hey {}", name)
+    } else {
+        println!("Sorry you're not on the list")
+    }
 }
